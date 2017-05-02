1dab4936f24438dc5e63b32dac7537a4625d7e64
vvl <lycwei@qq.com>
2017-05-02 15:59:16 +0800

    Initial commit

---
+++ src/org/vvl/main/java/Chapter01.java
@@ -0,0 +1,17 @@
+package org.vvl.main.java;
+
+import redis.clients.jedis.Jedis;
+
+public class Chapter01 {
+    public static void main(String[] args) {
+        new Chapter01().run();
+    }
+
+    public void run() {
+        Jedis conn = new Jedis("192.168.1.104", 6379);
+//        conn.select(15);
+        conn.set("foo", "bar");
+        System.out.println(conn.get("foo"));
+        conn.close();
+    }
+}
